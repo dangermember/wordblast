f6fdf030f589cfd38bc9a502c54822469dad6e66
Mohamed Eid Rashad <eng.mohamedeid2010@gmail.com>
2024-11-29 20:23:58 +0200

    completedWordsCount,SetSolved,resetGame

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { Devvit, useState } from '@devvit/public-api';
 import { GridComponent } from './GridComponent.js';
 import { IslandGridComponent } from './IslandGridComponent.js';
-import { generateGrid, analyzeGrid, generateLetterGrid} from './Helpers/BlockGenerationUtils.js';
+import { generateGrid, analyzeGrid, generateLetterGrid } from './Helpers/BlockGenerationUtils.js';
 import { groupWordsByLength, selectWordsBySizes } from './WordGenerationUtils.js';
 import Settings from './settings.js';
 import { IslandData } from './Interfaces/IslandData.js';
@@ -22,12 +22,12 @@ Devvit.addMenuItem({
     const { reddit, ui } = context;
     const subreddit = await reddit.getCurrentSubreddit();
     await reddit.submitPost({
-      title: 'My devvit post',
+      title: 'Word Blast',
       subredditName: subreddit.name,
       // The preview appears while the post loads
       preview: (
-        <vstack height="100%" width="100%" alignment="middle center">
-          <text size="large">Loading ...</text>
+        <vstack height="100%" width="100%" alignment="middle center" backgroundColor={Settings.GameBackGround}>
+          <text color={Settings.GameTextColor} size="large">Waiting For the fun to begin!</text>
         </vstack>
       ),
     });
@@ -39,21 +39,52 @@ const connectVerticies = () => {
 };
 // Add a post type definition
 Devvit.addCustomPostType({
-  name: 'Experience Post',
-  height: 'regular',
+  name: 'WordBlast 0.0',
+  height: 'tall',
   render: (_context) => {
-    const SetSolved = (cell: CellProps) => setCells((oldCells) => oldCells.map((row) => row.map((oldCell) => ({ ...oldCell, solved: oldCell.x == cell.x && oldCell.y == cell.y }))))
     const groupedWords = groupWordsByLength(["apple", "banana", "cherry", "date", "egg", "fig", "grape", "Desktop", "Toy", "Hen", "Paper", "Chair", "Bear", "Egg", "Cat", "Rat", "Chick"]);
     const [currentIsland, setCurrentIsland] = useState<IslandData | null>(null);
     // Example usage: block generation
-    const [grid, _setGrid] = useState(generateGrid(Settings.GridSize));
-    const [islands, _setIslands] = useState(analyzeGrid(grid));
-    const [sizes, _setSizes] = useState(islands.map(i => i.size));
-    const [selectedWords, _setSelectedWords] = useState(selectWordsBySizes(groupedWords, sizes));
-    const [letterGrid, _setLetterGrid] = useState(generateLetterGrid(grid, islands, selectedWords));
+    const [grid, setGrid] = useState(generateGrid(Settings.GridSize));
+    const [islands, setIslands] = useState(analyzeGrid(grid));
+    const [sizes, setSizes] = useState(islands.map(i => i.size));
+    const [selectedWords, setSelectedWords] = useState(selectWordsBySizes(groupedWords, sizes));
+    const [completedWordsCount, setCompletedWordsCount] = useState(0);
+    const [letterGrid, setLetterGrid] = useState(generateLetterGrid(grid, islands, selectedWords));
     const [cells, setCells] = useState(GridToCells(letterGrid));
-    
-    
+    const SetSolved = (cell: CellProps) => {
+      let isFirstCell = cell.x == currentIsland?.vertices[0][0] && cell.y == currentIsland?.vertices[0][1]
+      if (isFirstCell) {
+        let newcells = cells;
+        if (currentIsland?.vertices) {
+          for (let [x, y] of currentIsland?.vertices) {
+            newcells[x][y].solved = true;
+          }
+          setCells(newcells)
+          setCompletedWordsCount(old => old + 1)
+        }
+      }
+    }
+    const resetGame = () => {
+      let newGrid, newIslands, newSizes, newSelectedWords, newLetterGrid, newCells;
+      newGrid = generateGrid(Settings.GridSize);
+      newIslands = analyzeGrid(newGrid);
+      newSizes = newIslands.map(i => i.size);
+      console.log("groupedWords",groupedWords)
+      console.log("newSizes",newSizes)
+      newSelectedWords = selectWordsBySizes(groupedWords, newSizes);
+      console.log("newSelectedWords",newSelectedWords)
+      newLetterGrid = generateLetterGrid(newGrid, newIslands, newSelectedWords);
+      newCells = GridToCells(newLetterGrid);
+      setGrid(newGrid)
+      setIslands(newIslands)
+      setSizes(newSizes)
+      setSelectedWords(newSelectedWords)
+      setLetterGrid(newLetterGrid)
+      setCells(newCells)
+      setCompletedWordsCount(0)
+    }
+
     //console.log("\nIsland Data:");
     // islands.forEach((island) => {
     //   console.log(`Island ${island.index}: Size = ${island.size}, Vertices = ${JSON.stringify(island.vertices)}`);
@@ -75,9 +106,15 @@ Devvit.addCustomPostType({
     //console.log(JSON.stringify(islands));
 
     return (
-      <vstack alignment='center middle' height='100%' gap='large' backgroundColor={Settings.MainBackGround}>
+      <vstack alignment='center middle' height='100%' gap='large' backgroundColor={Settings.GameBackGround}>
+        <hstack alignment='center middle' gap="medium"> <button
+          appearance='success'
+          onPress={() => resetGame()}
+        >
+          New Game
+        </button> <text size="medium" color={Settings.GameTextColor}>Found Words {completedWordsCount} / {selectedWords.length}</text></hstack>
         <GridComponent Cells={cells} onCellClick={(Cell) => SetSolved(Cell)} />
-        <hstack gap='small' backgroundColor={Settings.IslandsBackGround} padding="small">
+        <hstack gap='small' backgroundColor={Settings.IslandsBackGround}>
           {islands.map((island) => <IslandGridComponent onClick={() => setCurrentIsland(island)} isHighlighted={currentIsland?.index === island.index} gridSize={5} islandVertices={island.vertices} />)}
         </hstack>
       </vstack>
